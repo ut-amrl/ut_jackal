f450cc1c3ae47d978be102ea9c4087f2c4e9270e
zdeng-UTexas <zdeng@utexas.edu>
2024-04-12 16:11:33 -0400

    update param

--- config/navigation.lua
+++ config/navigation.lua
@@ -5,7 +5,7 @@ end
 NavigationParameters = {
   laser_topics = {
     "/velodyne_2dscan",
-    "/kinect_laserscan"
+    -- "/kinect_laserscan"
   };
   laser_frame = "base_link";
   odom_topic = "/jackal_velocity_controller/odom";
@@ -14,30 +14,30 @@ NavigationParameters = {
   init_topic = "/initialpose";
   enable_topic = "/autonomy_arbiter/enabled";
   dt = 0.025;
-  max_linear_accel = 0.5;
+  max_linear_accel = 0.3;
   max_linear_decel = 2;
   max_linear_speed = 0.7;
   max_angular_accel = 0.7;
   max_angular_decel = 1;
-  max_angular_speed = 0.6; -- 0.5
-  carrot_dist = 3.5; -- 2.5
+  max_angular_speed = 0.5; -- 0.5
+  carrot_dist = 1.5; -- 2.5
   system_latency = 0.24;
-  obstacle_margin = 0.13;
-  num_options = 80; -- 41
+  obstacle_margin = 0.05;
+  num_options = 40; -- 41
   robot_width = 0.44;
-  robot_length = 1.0;
+  robot_length = 0.5;
   base_link_offset = 0;
-  max_free_path_length = 10.0; -- 6.0
+  max_free_path_length = 6.0; -- 6.0
   max_clearance = 1.0;
   can_traverse_stairs = false;
   evaluator_type = "linear";
   camera_calibration_path = "config/camera_calibration_kinect.yaml";
   model_path = "../preference_learning_models/jit_cost_model_outdoor_6dim.pt";
-  local_fov = deg2rad(90);
+  local_fov = deg2rad(40);
   target_dist_tolerance = 0.6;
   target_vel_tolerance = 0.1;
   target_angle_tolerance = 0.05;
-  use_map_speed = true;
+  use_map_speed = false;
   use_kinect = false;
 };
 
